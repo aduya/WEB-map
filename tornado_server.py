e4adb87a24a58563fdffe554512cdc7258ec2e8c
aduya <845331767@qq.com>
2017-05-05 21:15:03 +0800

    123.txt

--- tornado_server.py
+++ tornado_server.py
@@ -3,5 +3,5 @@ from tornado.httpserver import HTTPServer
 from tornado.ioloop import IOLoop
 from run import app
 http_server = HTTPServer(WSGIContainer(app))
-http_server.listen(88)
+http_server.listen(80)
 IOLoop.instance().start()
